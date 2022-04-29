0fb5a287f1c18c5bcea99ee0b3884d54ccc25084
alberchec <alberchec@hotmail.com>
2022-04-29 17:30:21 -0300

    change image quality

--- Images/ImageResize.py
+++ Images/ImageResize.py
@@ -6,8 +6,9 @@ del arguments[0]
 
 for file in arguments:
 	size=1500,1125
+	#size=800,600
 	image=Image.open(file)
 	image=image.resize(size)
-	image.save("mod_" + file,optimize=True,quality=60)
+	image.save("mod_" + file,optimize=True,quality=70)
 
 	image.close()
