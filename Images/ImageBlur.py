ea8bc545cf2b14043f6427d02231262d998f706f
alberchec <alberchec@hotmail.com>
2022-05-06 04:55:06 -0300

    add save quality and rm exif

--- Images/ImageBlur.py
+++ Images/ImageBlur.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageOps
 import sys
 
 arguments = sys.argv
@@ -6,8 +6,9 @@ del arguments[0]
 
 for file in arguments:
 	im = Image.open(file)
+	im=ImageOps.exif_transpose(im)
 	im_blur = im.filter(ImageFilter.GaussianBlur(radius=10) )
-	im_blur.save("mod_" + file)
+	im_blur.save("mod_" + file,optimize=True,quality=50)
 
 	im.close()
 	im_blur.close()
