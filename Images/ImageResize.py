7d676ddef1bcbedd5ba03d4df46c13b325769267
alberchec <alberchec@hotmail.com>
2022-05-06 04:48:28 -0300

    works for landscape and portrait, add gaussian blur and rm exif

--- Images/ImageResize.py
+++ Images/ImageResize.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageFilter, ImageOps
 import sys
 
 arguments = sys.argv
@@ -7,8 +7,17 @@ del arguments[0]
 for file in arguments:
 	size=1500,1125
 	#size=800,600
+	#size=200,150
+
 	image=Image.open(file)
+	image=ImageOps.exif_transpose(image)
+	pixels = image.size
+
+	if(pixels[0] < pixels[1]):
+		size = size[1],size[0]
+
 	image=image.resize(size)
-	image.save("mod_" + file,optimize=True,quality=70)
+	image = image.filter(ImageFilter.GaussianBlur(radius=0.5) )
+	image.save("mod_" + file,optimize=True,quality=80)
 
 	image.close()
